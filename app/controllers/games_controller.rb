626d88e931f52c603806395235478180e4269726
leon kranich <leon_kranich@yahoo.de>
2021-01-11 14:21:28 +0100

    implemented english_word API and finished first simple logic of the game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,5 @@
+require "open-uri"
+
 class GamesController < ApplicationController
 
   def new
@@ -5,6 +7,21 @@ class GamesController < ApplicationController
   end
 
   def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
 end
