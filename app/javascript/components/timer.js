02453a2b9061809617469a655eb90a086079ba51
leon kranich <leon_kranich@yahoo.de>
2021-01-13 15:48:22 +0100

    now innerHtml...and score for Game..

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,7 +1,6 @@
 const timer = () => {
   const startButton = document.getElementById("start-button");
   const endButton = document.getElementById("end-button");
-  
   startButton.addEventListener("click", (event) => {
     let startTime = performance.now();
     endButton.addEventListener("click", (event) => {
@@ -10,11 +9,9 @@ const timer = () => {
       timeDiff /= 1000;
       const seconds = timeDiff.toFixed(3)
       console.log(seconds + " seconds");
+      return seconds;
     });
   });
-
-
-  
 }
 
 export { timer };
