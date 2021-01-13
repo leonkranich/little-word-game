75152d167ad6289e92b7b10ca2b8fae344c33080
leon kranich <leon_kranich@yahoo.de>
2021-01-13 14:24:39 +0100

    basic timer

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,16 +1,20 @@
 const timer = () => {
   const startButton = document.getElementById("start-button");
   const endButton = document.getElementById("end-button");
-
+  
   startButton.addEventListener("click", (event) => {
-    const startTime = Date.now();
-    console.log(startTime);
+    let startTime = performance.now();
+    endButton.addEventListener("click", (event) => {
+      const endTime = performance.now();
+      let timeDiff = endTime - startTime;
+      timeDiff /= 1000;
+      const seconds = timeDiff.toFixed(3)
+      console.log(seconds + " seconds");
+    });
   });
 
-  endButton.addEventListener("click", (event) => {
-    const endTime = Date.now();
-    console.log(endTime)
-  });
+
+  
 }
 
 export { timer };
