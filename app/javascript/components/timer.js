111e5cd2ef290d18da20e5093e34d3af7d22bdd5
leon kranich <leon_kranich@yahoo.de>
2021-01-14 14:44:47 +0100

    will refactor it now into a single view app

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,17 +1,18 @@
 const timer = () => {
   const startButton = document.getElementById("start-button");
   const endButton = document.getElementById("end-button");
+
   startButton.addEventListener("click", (event) => {
     let startTime = performance.now();
     endButton.addEventListener("click", (event) => {
       const endTime = performance.now();
       let timeDiff = endTime - startTime;
       timeDiff /= 1000;
-      const seconds = timeDiff.toFixed(3)
-      console.log(seconds + " seconds");
-      return seconds;
+      const seconds = timeDiff.toFixed(3);
+      console.log(seconds);
     });
   });
+  
 }
 
 export { timer };
