48781f76a584b3fbc3290a699fd6507accb8c449
leon kranich <leon_kranich@yahoo.de>
2021-01-14 14:54:15 +0100

    little test

--- app/javascript/components/timer.js
+++ app/javascript/components/timer.js
@@ -1,18 +1,18 @@
-const timer = () => {
-  const startButton = document.getElementById("start-button");
-  const endButton = document.getElementById("end-button");
+// const timer = () => {
+//   const startButton = document.getElementById("start-button");
+//   const endButton = document.getElementById("end-button");
 
-  startButton.addEventListener("click", (event) => {
-    let startTime = performance.now();
-    endButton.addEventListener("click", (event) => {
-      const endTime = performance.now();
-      let timeDiff = endTime - startTime;
-      timeDiff /= 1000;
-      const seconds = timeDiff.toFixed(3);
-      console.log(seconds);
-    });
-  });
+//   startButton.addEventListener("click", (event) => {
+//     let startTime = performance.now();
+//     endButton.addEventListener("click", (event) => {
+//       const endTime = performance.now();
+//       let timeDiff = endTime - startTime;
+//       timeDiff /= 1000;
+//       const seconds = timeDiff.toFixed(3);
+//       console.log(seconds);
+//     });
+//   });
   
-}
+// }
 
-export { timer };
+// export { timer };
