fe65d78217ec2d78fe3a27b9263ba6f94b04ef10
leon kranich <leon_kranich@yahoo.de>
2021-01-17 15:37:38 +0100

    some tests

--- app/javascript/components/showResult.js
+++ app/javascript/components/showResult.js
@@ -2,12 +2,24 @@ import { wordIncluded } from "./wordIncluded";
 
 const showResult = () => {
   const result = document.getElementById("result");
+  // const form = document.getElementById("search-form");
+  // const included = wordIncluded();
+  // console.log(included);
+  
+  // form.addEventListener('submit', (event) => {
+  //   event.preventDefault(); // <-- to prevent <form>'s native behaviour
+  //   const input = event.currentTarget.querySelector('.form-control');
+  //   // results.innerHTML = '';
+  //   const word = input.value.toUpperCase();
+  // });
 
-  if (wordIncluded() === true) {
-    result.insertAdjacentHTML("beforeend", ` <p>You're a genuis!</p>`);
-  } else {
-    result.insertAdjacentHTML("beforeend", ` <p>dunmbass</p>`);
-  }
+
+  // if (included === true) {
+  //   result.insertAdjacentHTML("beforeend", ` <p>You're a genuis!</p>`);
+  // } else {
+  //   result.insertAdjacentHTML("beforeend", ` <p>dunmbass</p>`);
+  // }
+  
 }
 
 export { showResult };
