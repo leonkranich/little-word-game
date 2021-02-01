7b34610d0b0acb87bd382468970f3d487e8d520d
leon kranich <leon_kranich@yahoo.de>
2021-02-01 14:38:53 +0100

    everything is working, but some style would be nice

--- app/javascript/components/showResult.js
+++ app/javascript/components/showResult.js
@@ -6,22 +6,27 @@ const showResult = () => {
   const endButton = document.getElementById("end-submit");
   const letters = document.querySelector('.letters');
 
-  const timerAndStart = () => {
+  const start = () => {
     startButton.addEventListener("click", (event) => {
-      let startTime = performance.now();
       Resultarray.forEach((letter) => {
         const randomLetter = `<li>${letter}</li>`
         letters.insertAdjacentHTML("beforeend", randomLetter);
       });
-      endButton.addEventListener("click", (event) => {
-        const endTime = performance.now();
-        let timeDiff = endTime - startTime;
-        timeDiff /= 1000;
-        const seconds = timeDiff.toFixed(3);
-        time.insertAdjacentHTML('afterbegin', ` <p>Wow it took you only ${seconds}!</p>`);
-      });
     });
   }
+
+  
+  startButton.addEventListener("click", (event) => {
+    let startTime = performance.now();
+    endButton.addEventListener("click", (event) => {
+      const endTime = performance.now();
+      let timeDiff = endTime - startTime;
+      timeDiff /= 1000;
+      const seconds = timeDiff.toFixed(3);
+      time.insertAdjacentHTML('afterbegin', ` <p>Wow it took you only ${seconds} seconds!</p>`);
+    });
+  });
+  
 
   const searchWord = (word, isIncluded) => {
     fetch(`https://wagon-dictionary.herokuapp.com/${word}`)
@@ -55,8 +60,8 @@ const showResult = () => {
     searchWord(inputWord, included);
   });
 
-  timerAndStart();
-  
+  start();
+
 }
 
 export { showResult };
