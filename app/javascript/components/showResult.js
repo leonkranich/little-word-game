bc067bddfe23eb3364fff17b3f9eee575dc18c53
leon kranich <leon_kranich@yahoo.de>
2021-02-01 14:11:39 +0100

    little adjustment

--- app/javascript/components/showResult.js
+++ app/javascript/components/showResult.js
@@ -23,43 +23,40 @@ const showResult = () => {
     });
   }
 
-
-  const wordIncluded = () => {
-    const searchWord = (word, isIncluded) => {
-      fetch(`https://wagon-dictionary.herokuapp.com/${word}`)
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data['found']);
-        console.log(word);
-        console.log(isIncluded);
-        if (isIncluded === true) {
-          if (data['found'] === true) {
-            result.insertAdjacentHTML('afterbegin', ` <p>You are <strong>genuis!!! ${word}</strong> is a valid English word</p>`);
-          } else {
-            result.insertAdjacentHTML('afterbegin', ` <p>Sorry but <strong>${word}</strong> does not seem to be a valid English word...dumbass</p>`);
-          }
+  const searchWord = (word, isIncluded) => {
+    fetch(`https://wagon-dictionary.herokuapp.com/${word}`)
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data['found']);
+      console.log(word);
+      console.log(isIncluded);
+      if (isIncluded === true) {
+        if (data['found'] === true) {
+          result.insertAdjacentHTML('afterbegin', ` <p>You are <strong>genuis!!! ${word}</strong> is a valid English word</p>`);
         } else {
-          result.insertAdjacentHTML('afterbegin', ` <p>Dunmbass! You can't build <strong>${word}</strong> with the letters <strong>${Resultarray.join('-')}</strong></p>`);
+          result.insertAdjacentHTML('afterbegin', ` <p>Sorry but <strong>${word}</strong> does not seem to be a valid English word...dumbass</p>`);
         }
-      });
-    };
-    const allFound = (wo, ar) => {
-        return wo.every( letter => ar.includes(letter) );
-      };
-
-    form.addEventListener('submit', (event) => {
-      event.preventDefault(); // <-- to prevent <form>'s native behaviour
-      const input = event.currentTarget.querySelector('.form-control');
-      const inputWord = input.value;
-      const wordUp = inputWord.toUpperCase();
-      const wordArray = wordUp.split('');
-      const included = allFound(wordArray, Resultarray);
-      searchWord(inputWord, included);
+      } else {
+        result.insertAdjacentHTML('afterbegin', ` <p>Dunmbass! You can't build <strong>${word}</strong> with the letters <strong>${Resultarray.join('-')}</strong></p>`);
+      }
     });
-  }
+  };
+  const allFound = (wo, ar) => {
+      return wo.every( letter => ar.includes(letter) );
+    };
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault(); // <-- to prevent <form>'s native behaviour
+    const input = event.currentTarget.querySelector('.form-control');
+    const inputWord = input.value;
+    const wordUp = inputWord.toUpperCase();
+    const wordArray = wordUp.split('');
+    const included = allFound(wordArray, Resultarray);
+    searchWord(inputWord, included);
+  });
 
   timerAndStart();
-  wordIncluded();
+  
 }
 
 export { showResult };
